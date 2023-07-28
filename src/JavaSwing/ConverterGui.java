de1cef9f6ea6e62acef8fa926c3866fed451a6d8
Mauricio Cardona <maucaralar@gmail.com>
2023-07-27 23:37:02 -0400

    added elements

--- src/JavaSwing/ConverterGui.java
+++ src/JavaSwing/ConverterGui.java
@@ -5,34 +5,70 @@ import java.awt.*;
 
 public class ConverterGui extends JFrame {
 
-    private JMenuBar menuBar;
+    JFrame frame;
+    JMenuBar menuBar;
+    JMenu fileMenu;
 
     public ConverterGui(){
-        JFrame frame =  new JFrame();  //create the jframe obj
+//        try {
+//            // Set Nimbus look and feel
+//            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+//        Frame setup
+        frame =  new JFrame("Converter 1.0");  //create the jframe obj
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set the frame to close when hit the exit button
+        frame.setSize(400, 400);
+//        frame.setTitle();
+
+        JMenuBar colorBar = new JMenuBar();
+        colorBar.setOpaque(true);
+        colorBar.setBackground(new Color(232, 11, 66));
+        colorBar.setPreferredSize(new Dimension(400, 30));
+
+
+        JLabel yellowLabel = new JLabel();
+        yellowLabel.setOpaque(true);
+        yellowLabel.setBackground(new Color(2, 5, 245));
+        yellowLabel.setPreferredSize(new Dimension(200, 180));
+
+        fileMenu = new JMenu("sdakbakbfa");
+        colorBar.add(fileMenu);
+
+
+        frame.setJMenuBar(colorBar);
+        frame.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+
+
+
+
+//        JPanel panel1 = new JPanel(); // create a panel1
+//        panel1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // set the borders of the panel1 (margin on
+//        // css)
+//        panel1.setLayout(new GridLayout(6,2)); // define a layout style.
+////        panel1.setPreferredSize(new Dimension(50, 50));
+//        panel1.add(menuBar);
+////        frame.pack(); // pack makes the window big enough for fitting all the elements.
+//        frame.add(panel1, BorderLayout.WEST); // add the panel1 to the frame.
+
+
+//        JPanel panel2 = new JPanel();
+//        panel2.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+//        panel2.setLayout(new GridLayout(6, 2));
+////        panel2.add(menuBar);
+
+
+//        frame.add(panel2, BorderLayout.EAST);
+//
+//        showMenu();
+        frame.pack();
         frame.setVisible(true); // make it visible
-        frame.setSize(500, 500);
-        frame.setTitle("Converter 1.0");
-
-
-        JPanel panel1 = new JPanel(); // create a panel1
-        panel1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // set the borders of the panel1 (margin on
-        // css)
-        panel1.setLayout(new GridLayout(2,2)); // define a layout style.
-        panel1.setPreferredSize(new Dimension(100, 100));
-        panel1.add(menuBar);
-//        frame.pack(); // pack makes the window big enough for fitting all the elements.
-
-        frame.add(panel1, BorderLayout.CENTER); // add the panel1 to the frame.
-
-
-
-        JPanel panel2 = new JPanel();
-        showMenu();
 
     }
     public void showMenu(){
-        menuBar = new JMenuBar();
+
         JMenu menu = new JMenu("Distance: ");
         //make an array list with the options!!
         JMenuItem option1 = new JMenuItem("Centimeters to meters");
@@ -54,8 +90,8 @@ public class ConverterGui extends JFrame {
 
     public static void main(String[] args) {
 //        SwingUtilities.invokeLater(ConverterGui::new);
-        ConverterGui window = new ConverterGui();
-             
+//        ConverterGui window = new ConverterGui();
+          SwingUtilities.invokeLater(ConverterGui::new);
 
  
     }
